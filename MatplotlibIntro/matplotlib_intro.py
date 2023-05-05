701e038b05bad89502500617530a7857ea365e36
Griffin <brandstg@oregonstate.edu>
2023-05-05 17:49:37 +0000

    lab 4

--- MatplotlibIntro/matplotlib_intro.py
+++ MatplotlibIntro/matplotlib_intro.py
@@ -1,10 +1,12 @@
 # matplotlib_intro.py
 """Python Essentials: Intro to Matplotlib.
-<Name>
-<Class>
-<Date>
+Griffin Brandstetter
+MTH 420
+4/28/23
 """
 
+import numpy as np
+from matplotlib import pyplot as plt
 
 # Problem 1
 def var_of_means(n):
@@ -18,13 +20,22 @@ def var_of_means(n):
     Returns:
         (float) The variance of the means of each row.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    A = np.random.normal(size=(n,n))
+    M = np.mean(A, axis=1)
+    V = np.var(M)
+    return(V)
+
+    
 
 def prob1():
     """ Create an array of the results of var_of_means() with inputs
     n = 100, 200, ..., 1000. Plot and show the resulting array.
     """
-    raise NotImplementedError("Problem 1 Incomplete")
+    P = [var_of_means(100), var_of_means(200), var_of_means(300), var_of_means(400), var_of_means(500), var_of_means(600), var_of_means(700), var_of_means(800), var_of_means(900), var_of_means(1000)]
+    plt.plot(P) 
+    plt.show()  
+    
+prob1()
 
 
 # Problem 2
@@ -33,7 +44,14 @@ def prob2():
     [-2pi, 2pi]. Make sure the domain is refined enough to produce a figure
     with good resolution.
     """
-    raise NotImplementedError("Problem 2 Incomplete")
+    x = np.linspace(-2*np.pi, 2*np.pi, 100)
+    cos = np.cos(x)
+    sin = np.sin(x)
+    plt.plot(x, cos)
+    plt.plot(x, sin)
+    plt.show()
+    
+prob2()
 
 
 # Problem 3
@@ -44,7 +62,17 @@ def prob3():
         3. Set the range of the x-axis to [-2,6] and the range of the
            y-axis to [-6,6].
     """
-    raise NotImplementedError("Problem 3 Incomplete")
+    x1 = np.linspace(-2, 1, 40, endpoint=False)
+    f1 = 1 / (x1-1)
+    x2 = np.linspace(1, 6, 40)[1:]
+    f2 = 1 / (x2-1)
+    plt.plot(x1, f1, 'm:', lw = 4)
+    plt.plot(x2, f2, 'm:', lw = 4)
+    plt.xlim(-2, 6)
+    plt.ylim(-6, 6)
+    plt.show()
+    
+prob3()
 
 
 # Problem 4
@@ -61,7 +89,28 @@ def prob4():
              2sin(x): blue dashed line.
             2sin(2x): magenta dotted line.
     """
-    raise NotImplementedError("Problem 4 Incomplete")
+    x = np.linspace(0, 2*np.pi, 100)
+    xmin = 0
+    xmax = 2*np.pi
+    ymin = -2
+    ymax = 2
+    fig, axes = plt.subplots(2, 2)
+    fig.suptitle('Some Sines')
+    axes[0,0].plot(x, np.sin(x), 'g')
+    axes[0,0].set_title('Plot A')
+    axes[0,0].axis([xmin, xmax, ymin, ymax]) 
+    axes[0,1].plot(x, np.sin(2*x), 'r--')
+    axes[0,1].set_title('Plot B')
+    axes[0,1].axis([xmin, xmax, ymin, ymax]) 
+    axes[1,0].plot(x, 2*np.sin(x), 'b--')
+    axes[1,0].set_title('Plot C')
+    axes[1,0].axis([xmin, xmax, ymin, ymax]) 
+    axes[1,1].plot(x, 2*np.sin(2*x), 'm:')
+    axes[1,1].set_title('Plot D')
+    axes[1,1].axis([xmin, xmax, ymin, ymax]) 
+    plt.show()
+
+prob4()
 
 
 # Problem 5
@@ -74,7 +123,16 @@ def prob5():
         2. A histogram of the hours of the day, with one bin per hour.
             Label and set the limits of the x-axis.
     """
-    raise NotImplementedError("Problem 5 Incomplete")
+    '''data = np.load('FARS.npy')
+    ax1 = plt.subplot(121)
+    ax2 = plt.subplot(122)
+    ax1.plot(data[:,1], data[:,2], 'k', marker=',',lw=0, linestyle="")
+    ax1.axis("equal")
+    ax1.set_xlabel('Longitude')
+    ax1.set_ylabel('Latitude')
+    ax2.hist(data[:,0],bins = 24)
+    ax2.set_xlabel('Time of Day')
+    plt.show'''
 
 
 # Problem 6
@@ -88,4 +146,23 @@ def prob6():
         3. Choose a non-default color scheme.
         4. Include a color scale bar for each subplot.
     """
-    raise NotImplementedError("Problem 6 Incomplete")
+    x = np.linspace(-np.pi, np.pi, 100)
+    y = x.copy()
+    X, Y = np.meshgrid(x, y)
+    Z = np.sin(X) * np.sin(Y) / (X*Y)
+    plt.subplot(221)
+    plt.pcolormesh(X, Y, Z, cmap="viridis", shading="auto")
+    plt.colorbar()
+    plt.xlim(-np.pi, np.pi)
+    plt.ylim(-np.pi, np.pi)
+    plt.axis("equal")
+    
+    plt.subplot(222)
+    plt.contour(X, Y, Z, 10, cmap="coolwarm")
+    plt.colorbar()
+
+    
+    plt.show
+
+prob6()
+
